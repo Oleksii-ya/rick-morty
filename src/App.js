155c1ac2ded6e6256e7ce234ab1b6eb1abacb10e
Oleksii-ya <prog8dev@gmail.com>
2023-03-30 15:38:01 +0300

    nav characters

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import { Routes, Route } from 'react-router-dom';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Container, CssBaseline, ThemeProvider, Typography } from '@mui/material';
 import './App.css';
 
 import CharactersList from './pages/CharactersList';
 import Character from './pages/Character';
 import List from './pages/components/List';
 import theme from './pages/styles';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [total, setTotal] = useState(null)
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character/")
+      .then(res => res.json())
+      .then(data => setTotal(data.info.count))
+  }, [])
+
+  if (!total) {
+    return <Container maxWidth="lg">
+      <Typography variant="body1">Loading...</Typography>
+    </Container>
+  }
   return (
     <>
       <CssBaseline />
       <ThemeProvider theme={theme}>
         <Routes>
-          <Route path="/" element={<CharactersList />}>
+          <Route path="/" element={<CharactersList total={total} />}>
             <Route path=":page" element={<List />} />
           </Route>
-          <Route path="/character/:id" element={<Character />} />
+          <Route path="/character/:id" element={<Character total={total} />} />
         </Routes>
       </ThemeProvider>
     </>
